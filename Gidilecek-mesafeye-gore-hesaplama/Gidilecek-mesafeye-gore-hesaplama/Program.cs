e1d4f7125da3214cf094a89a68c0e7e7451af876
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2023-02-19 19:29:31 +0300

    Add files via upload

---
+++ Gidilecek-mesafeye-gore-hesaplama/Gidilecek-mesafeye-gore-hesaplama/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double mesafe = 0;
+        int zaman = 0;
+        double hiz = 0;
+        Console.WriteLine("Gidilecek mesafeye göre hız hesaplayan program ");
+
+        Console.WriteLine("\n Gidilecek mesafe (m) : ");
+        mesafe= Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Mesafeniz : ",mesafe);
+
+        Console.WriteLine("\n Varis zamani (sn): ");
+        zaman = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Varis zamaniniz : ", zaman);
+
+        hiz = (double) mesafe / zaman;
+
+        Console.WriteLine("\n Hiziniz : {0} m/s olmalidir.", hiz);
+
+    }
+}
